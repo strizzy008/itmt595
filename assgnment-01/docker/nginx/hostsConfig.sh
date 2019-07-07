0873e8b6ca2a10938707593aec78d2a9b30248a5
vagrant <vagrant@dockerp.vm>
2019-07-07 13:25:29 -0500

    updating docker configs

---
+++ assgnment-01/docker/nginx/hostsConfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "192.168.50.11  nginx-web-server" >> /etc/hosts
+echo "192.168.50.12  node-application-server" >> /etc/hosts
+echo "192.168.50.13  mongodb-server" >> /etc/hosts
+echo "192.168.50.14  mongodb-rep1-server" >> /etc/hosts
+echo "192.168.50.15  redis-caching-server" >> /etc/hosts
