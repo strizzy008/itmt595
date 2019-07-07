51d91b9df4d67af3a59a707086334cd98d371315
vagrant <vagrant@dockerp.vm>
2019-07-07 18:54:28 -0500

    updated node

--- assgnment-01/docker/node/set-up-node2.sh
+++ assgnment-01/docker/node/set-up-node2.sh
@@ -14,12 +14,11 @@
 #sudo apt-get install git
 
 # Add derver to hostname
-sudo chown vagrant /etc/hosts
-echo "192.168.50.11  nginx-web-server" >> /etc/hosts
-echo "192.168.50.12  node-application-server" >> /etc/hosts
-echo "192.168.50.13  mongodb-server" >> /etc/hosts
-echo "192.168.50.14  mongodb-rep1-server" >> /etc/hosts
-echo "192.168.50.15  redis-caching-server" >> /etc/hosts
+#echo "192.168.50.11  nginx-web-server" >> /etc/hosts
+#echo "192.168.50.12  node-application-server" >> /etc/hosts
+#echo "192.168.50.13  mongodb-server" >> /etc/hosts
+#echo "192.168.50.14  mongodb-rep1-server" >> /etc/hosts
+#echo "192.168.50.15  redis-caching-server" >> /etc/hosts
 
 # Install the NodeSource PPA in order to get access to its contents into the home directory
 #cd ~
